0dfe25eef02347dc40d2d05ed6b7024865531dfc
greysonmrx <greysonmrx@gmail.com>
2020-01-26 20:57:49 -0300

    test: user registration with duplicated email

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -15,4 +15,26 @@ describe('User', () => {
 
     expect(response.body).toHaveProperty('id');
   });
+
+  it('should not be able to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        first_name: 'Greyson',
+        last_name: 'Filho',
+        email: 'greysonmrx@gmail.com',
+        password: '12345'
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        first_name: 'Greyson',
+        last_name: 'Filho',
+        email: 'greysonmrx@gmail.com',
+        password: '12345'
+      });
+
+    expect(response.status).toBe(400);
+  });
 });
