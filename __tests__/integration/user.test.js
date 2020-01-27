457035fac31cf809787089d20e8980980339e57d
greysonmrx <greysonmrx@gmail.com>
2020-01-26 21:14:13 -0300

    test: encrypt user password

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,6 +1,8 @@
+import bcrypt from 'bcryptjs';
 import request from 'supertest';
 
 import app from '../../src/app';
+import User from '../../src/app/models/User';
 import truncate from '../utils/truncate';
 
 describe('User', () => {
@@ -41,5 +43,18 @@ describe('User', () => {
       });
 
     expect(response.status).toBe(400);
+  });
+
+  it('should encrypt user password when new user created', async () => {
+    const user = await User.create({
+      first_name: 'Greyson',
+      last_name: 'Filho',
+      email: 'greysonmrx@gmail.com',
+      password: '12345'
+    });
+
+    const compareHash = await bcrypt.compare('12345', user.password);
+
+    expect(compareHash).toBe(true);
   });
 });
